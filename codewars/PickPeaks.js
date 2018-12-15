bcc266af093bb67a07cd7552ef6a990b3c2db0ec
Ashwanth A R <ashwanth1109@gmail.com>
2018-12-16 00:19:38 +0530

    Added cool solutions by others on codewars

--- codewars/PickPeaks.js
+++ codewars/PickPeaks.js
@@ -69,8 +69,65 @@ pickPeaks1 = arr => {
         }
     ));
 };
+//===========================================
+// RUNNING INTO SOME ISSUE WITH DEFINING INITIAL VALUE
+// COME BACK TO THIS
+//===========================================
 
 //===========================================
 // TEST CASE
 //===========================================
 console.log(pickPeaks1([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3]));
+
+//===========================================
+// COOL SOLUTIONS BY OTHERS
+//===========================================
+function pickPeaks2(arr) {
+    var result = { pos: [], peaks: [] };
+    if (arr.length > 2) {
+        var pos = -1;
+        for (var i = 1; i < arr.length; i++) {
+            if (arr[i] > arr[i - 1]) {
+                pos = i;
+            } else if (arr[i] < arr[i - 1] && pos != -1) {
+                result.pos.push(pos);
+                result.peaks.push(arr[pos]);
+                pos = -1;
+            }
+        }
+    }
+    return result;
+}
+
+function pickPeaks3(arr) {
+    var pos = arr
+        .map((x, i) => (i > 0 ? Math.sign(x - arr[i - 1]) * i : 0))
+        .filter(i => i != 0)
+        .filter((x, i, a) => i < a.length - 1 && (a[i + 1] < 0 && x > 0));
+    return { pos: pos, peaks: pos.map(i => arr[i]) };
+}
+
+function pickPeaks(arr) {
+    var up = 0;
+    return arr.reduce(
+        function(theCount, currentValue, currentIndex, array) {
+            if (
+                up != 0 &&
+                currentIndex > 0 &&
+                array[currentIndex + 1] < currentValue
+            ) {
+                theCount.pos.push(up);
+                theCount.peaks.push(currentValue);
+                up = 0;
+                //down here
+            }
+            if (array[currentIndex + 1] > currentValue) {
+                up = currentIndex + 1;
+                //up coming
+            }
+
+            return theCount;
+        },
+        { pos: [], peaks: [] }
+    );
+}
