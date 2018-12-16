4daa7fb8615eb6fccff5d2682bff34e33db4feb0
Ashwanth A R <ashwanth1109@gmail.com>
2018-12-16 14:29:47 +0530

    Solved FormatDuration.js

--- codewars/FormatDuration.js
+++ codewars/FormatDuration.js
@@ -9,7 +9,13 @@ formatDuration = (time, formatArray = []) => {
     //===========================================
     // Function to format string and add it to beginning of array
     //===========================================
-    const formatString = (arr, time, str, val, divFactor) => {
+    const formatString = (
+        arr,
+        time,
+        str,
+        divFactor,
+        val = time % divFactor
+    ) => {
         if (val === 0) {
             time = time / divFactor;
             return [arr, time];
@@ -21,49 +27,66 @@ formatDuration = (time, formatArray = []) => {
     //===========================================
     // Calculating seconds
     //===========================================
-    [formatArray, time] = formatString(
-        formatArray,
-        time,
-        "second",
-        time % 60,
-        60
-    );
+    [formatArray, time] = formatString(formatArray, time, "second", 60);
     //===========================================
     // Calculating minutes
     //===========================================
-    [formatArray, time] = formatString(
-        formatArray,
-        time,
-        "minute",
-        time % 60,
-        60
-    );
+    [formatArray, time] = formatString(formatArray, time, "minute", 60);
     //===========================================
     // Calculating hours
     //===========================================
-    [formatArray, time] = formatString(
-        formatArray,
-        time,
-        "hour",
-        time % 24,
-        24
-    );
+    [formatArray, time] = formatString(formatArray, time, "hour", 24);
     //===========================================
     // Calculating days
     //===========================================
-    [formatArray, time] = formatString(
-        formatArray,
-        time,
-        "day",
-        time % 365,
-        365
-    );
+    [formatArray, time] = formatString(formatArray, time, "day", 365);
     //===========================================
     // Calculating years
     //===========================================
-    [formatArray, time] = formatString(formatArray, time, "year", time, 1);
-    return formatArray;
+    if (time !== 0)
+        time === 1
+            ? formatArray.unshift(handleOne("year"))
+            : formatArray.unshift(`${time} years`);
+    //===========================================
+    // COMBINE PARTS OF ARRAY WITH COMMAS AND 'AND'
+    //===========================================
+    if (formatArray.length > 1) {
+        const lastTwoParts = [formatArray.pop(), formatArray.pop()];
+        formatArray.push(`${lastTwoParts[1]} and ${lastTwoParts[0]}`);
+    }
+    return formatArray.join(", ");
 };
+
+//===========================================
+// COOL SOLUTIONS BY OTHERS
+//===========================================
+function formatDuration(seconds) {
+    var time = {
+            year: 31536000,
+            day: 86400,
+            hour: 3600,
+            minute: 60,
+            second: 1
+        },
+        res = [];
+
+    if (seconds === 0) return "now";
+
+    for (var key in time) {
+        if (seconds >= time[key]) {
+            var val = Math.floor(seconds / time[key]);
+            res.push((val += val > 1 ? " " + key + "s" : " " + key));
+            seconds = seconds % time[key];
+        }
+    }
+
+    return res.length > 1
+        ? res.join(", ").replace(/,([^,]*)$/, " and" + "$1")
+        : res[0];
+}
+//===========================================
+// NICE. . . I SHOULD LEARN TO USE REGEX
+//===========================================
 
 //===========================================
 // TEST CASES
