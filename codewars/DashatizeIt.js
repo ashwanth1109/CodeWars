311c07fba560839c0dc2fadd448d7dd017a0676f
Ashwanth A R <ashwanth1109@gmail.com>
2018-12-18 12:44:24 +0530

    Solved DashatizeIt.js

--- codewars/DashatizeIt.js
+++ codewars/DashatizeIt.js
@@ -5,25 +5,49 @@
 // 6 kyu
 //===========================================
 
-dashatize = num => {
+dashatize0 = num => {
     const splitNum = (num, numArr = [], modArr = [num % 10, ...numArr]) =>
         num > 9 ? splitNum(Math.floor(num / 10), modArr) : modArr;
-    const numArr = splitNum(num);
+    const numArr = splitNum(Math.abs(num));
     const isEven = n => (n % 2 === 0 ? true : false);
     return numArr.reduce((acc, cur, id, src) => {
         if (isEven(cur)) {
+            // even numbers
             return acc + cur;
         } else {
+            // odd numbers
             if (id === 0) {
-                return acc + cur + "-";
-            } else if (id === src.length - 1) {
-                return isEven(src[src.length - 2])
-                    ? acc + "-" + cur
-                    : acc + cur;
+                // 1st number is odd
+                return isEven(src[1]) ? acc + cur + "-" : acc + cur; // check if 2nd num is even
             } else {
+                return isEven(src[id + 1])
+                    ? acc + "-" + cur + "-"
+                    : acc + "-" + cur;
             }
         }
     }, "");
+};
+
+//===========================================
+// REFACTORED SOLN - BYE BYE CODE READABILITY
+//===========================================
+dashatize = n => {
+    const split = (n, arr1 = [], arr2 = [n % 10, ...arr1]) =>
+        n > 9 ? split(Math.floor(n / 10), arr2) : arr2; // func to split n into its digits
+    const isEven = n => (n % 2 === 0 ? true : false); // func to check if n is even
+    return split(Math.abs(n)).reduce(
+        (acc, cur, id, src) =>
+            isEven(cur)
+                ? acc + cur // number is even
+                : id === 0 // if 1st num is odd
+                ? isEven(src[1]) // check if 2nd is even
+                    ? acc + cur + "-" // if 2nd num is even
+                    : acc + cur // if 2nd num is odd
+                : isEven(src[id + 1]) // if not 1st num
+                ? acc + "-" + cur + "-" // if next num is even
+                : acc + "-" + cur, // if next num is odd
+        ""
+    );
 };
 
 //===========================================
@@ -31,3 +55,26 @@ dashatize = num => {
 //===========================================
 console.log(dashatize(274));
 console.log(dashatize(6815));
+console.log(dashatize(1));
+
+//===========================================
+// COOL SOLUTIONS BY OTHERS - 1
+//===========================================
+function dashatize(num) {
+    return String(num)
+        .replace(/([13579])/g, "-$1-")
+        .replace(/--+/g, "-")
+        .replace(/(^-|-$)/g, "");
+}
+
+//===========================================
+// COOL SOLUTIONS BY OTHERS - 2
+//===========================================
+function dashatize(num) {
+    return isNaN(num)
+        ? "NaN"
+        : num
+              .toString()
+              .match(/([13579]|[02468]+)/g)
+              .join("-");
+}
