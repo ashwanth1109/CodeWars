4fbb2297b8a4abc2893bf6ec59ffba83bf32cbe8
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-27 02:04:22 +0530

    Tested object destructuring being passed into a function

--- test.js
+++ test.js
@@ -10,3 +10,15 @@
 // ------------------------------------------------------------
 // APPARENTLY THIS IS CALLED CURRYING - INTERESTING
 // ------------------------------------------------------------
+
+// ------------------------------------------------------------
+// Object Destructuring with default values in a function
+// ------------------------------------------------------------
+// const testFunc = ({ obj1 = 1, obj2 = 2 } = {}) => {
+//     console.log(obj1);
+//     console.log(obj2);
+// };
+
+// testFunc();
+// testFunc({ obj1: 3 });
+// testFunc({ obj1: 4, obj2: 5 });
