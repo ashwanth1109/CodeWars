9fc7e2400faa1e78f1983641c00bca2b9660ed1c
Ashwanth A R <ashwanth1109@gmail.com>
2019-09-18 22:50:28 +0530

    Completed Challenge 2 -
    https://dev.to/thepracticaldev/daily-challenge-2-string-diamond-21n2

---
+++ python/challenge2.py
@@ -0,0 +1,24 @@
+def diamond(num):
+    if (num % 2 == 0 or num < 0):
+        return None
+    else:
+        i = 1
+        goingUp = True
+        while i > 0:
+            for x in range(int((num - i) / 2)):
+                print(" ", end="")
+            for x in range(i):
+                print("*", end="")
+            for x in range(int((num - i) / 2)):
+                print(" ", end="")
+            print("\n")
+            if (i < num and goingUp):
+                i += 2
+            elif goingUp:
+                goingUp = False
+                i -= 2
+            else:
+                i -= 2
+
+
+diamond(5)
